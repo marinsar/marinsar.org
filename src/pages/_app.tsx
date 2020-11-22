af60022c0d64ed8fc668190571ed7026b938a68a
Christopher Henn <chris@chrishenn.net>
2020-11-22 08:59:19 -0800

    Move source files into src/

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import '../index.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
