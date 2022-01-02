c6c2a755b7bcb55e618bb3bd5d6a21731433790d
Christopher Henn <chris@chrishenn.net>
2022-01-02 16:39:01 -0500

    Add fade transitions to ImageGallery component

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,48 +1,107 @@
-import { FunctionComponent, useEffect, useState } from 'react';
+import { FunctionComponent, useEffect, useState, useCallback } from 'react';
 import Image from 'next/image';
 
 import type { Image as ImageType } from '../lib/api';
 
-const TRANSITION_MS = 6000;
+type State = 'A_VISIBLE' | 'B_VISIBLE' | 'A_FADING_IN' | 'B_FADING_IN';
+
+const NEXT_IMAGE_MS = 6000;
+const FADE_TRANSITION_MS = 1000;
+const VISIBLE_CLASSES = 'transition-opacity opacity-100 duration-1000';
+const INVISIBLE_CLASSES = 'transition-opacity opacity-0 duration-1000';
 
 export const ImageGallery: FunctionComponent<{ images: ImageType[] }> = ({
   images,
 }) => {
-  const [index, setIndex] = useState(0);
-  const nextIndex = (index + 1) % images.length;
+  const [state, setState] = useState<State>('A_FADING_IN');
+
+  const [aIndex, setAIndex] = useState(0);
+  const [bIndex, setBIndex] = useState(0);
+
+  const aImage = images[aIndex];
+  const bImage = images[bIndex];
+
+  const isAVisible = state === 'A_FADING_IN' || state === 'A_VISIBLE';
+  const isBVisible = state === 'B_FADING_IN' || state === 'B_VISIBLE';
+
+  const incIndex = useCallback((i: number) => (i + 1) % images.length, [
+    images.length,
+  ]);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIndex(nextIndex);
-    }, TRANSITION_MS);
+    let timeout: any;
+
+    switch (state) {
+      case 'A_FADING_IN': {
+        timeout = setTimeout(() => {
+          setState('A_VISIBLE');
+        }, FADE_TRANSITION_MS);
+
+        break;
+      }
+
+      case 'A_VISIBLE': {
+        setBIndex(incIndex(aIndex));
+
+        timeout = setTimeout(() => {
+          setState('B_FADING_IN');
+        }, NEXT_IMAGE_MS);
+
+        break;
+      }
+
+      case 'B_FADING_IN': {
+        timeout = setTimeout(() => {
+          setState('B_VISIBLE');
+        }, FADE_TRANSITION_MS);
+
+        break;
+      }
+
+      case 'B_VISIBLE': {
+        setAIndex(incIndex(bIndex));
+
+        timeout = setTimeout(() => {
+          setState('A_FADING_IN');
+        }, NEXT_IMAGE_MS);
+
+        break;
+      }
+
+      default: {
+        const unexpectedState: never = state;
+        throw new Error(`unexpected state "${unexpectedState as string}"`);
+      }
+    }
 
     return () => {
       clearTimeout(timeout);
     };
-  }, [nextIndex, setIndex, images]);
-
-  const visibleImage = images[index];
-  const nextImage = images[nextIndex];
+  }, [state, aIndex, bIndex, incIndex]);
 
   return (
-    <div className='relative w-full h-full'>
-      <div className='hidden'>
-        <Image
-          className='object-cover'
-          src={nextImage.url}
-          alt={nextImage.title}
-          width={nextImage.width}
-          height={nextImage.height}
-          layout='responsive'
-        />
-      </div>
+    <div className='relative w-full aspect-video'>
       <Image
-        className='object-cover'
-        src={visibleImage.url}
-        alt={visibleImage.title}
-        width={visibleImage.width}
-        height={visibleImage.height}
-        layout='responsive'
+        className={isAVisible ? VISIBLE_CLASSES : INVISIBLE_CLASSES}
+        src={aImage.url}
+        alt={aImage.title}
+        width={aImage.width}
+        height={aImage.height}
+        layout='fill'
+        objectFit='cover'
+        priority={true}
+        role='presentation'
+      />
+      <Image
+        className={isBVisible ? VISIBLE_CLASSES : INVISIBLE_CLASSES}
+        src={bImage.url}
+        alt={bImage.title}
+        width={bImage.width}
+        height={bImage.height}
+        layout='fill'
+        objectFit='cover'
+        priority={true}
+        role='presentation'
       />
     </div>
   );
